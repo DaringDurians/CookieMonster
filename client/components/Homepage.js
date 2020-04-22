1d9287130854f2be7fde2344f7e4ea42a5e77651
Tyler Eaden <tyler.eaden@gmail.com>
2020-04-22 15:07:01 -0400

    Fixed Brownie Route Bug + Created Homepage React View

---
+++ client/components/Homepage.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export const Homepage = props => {
+  return (
+    <div id="homepageBox">
+      {/* <!-- See navbar.js for reference of what will show up above --> */}
+      <div id="aboutUs">
+        <div>
+          <p>Welcome Message</p>
+        </div>
+        <div>
+          <img src="tbd" alt="tbd" />
+        </div>
+      </div>
+      <div id="cookieCategory">
+        <div>
+          <h3>Cookie Title</h3>
+        </div>
+        <div>
+          <img src="tbd" alt="tbd" />
+        </div>
+      </div>
+      <div id="brownieCategory">
+        <div>
+          <h3>Brownie Title</h3>
+        </div>
+        <div>
+          <img src="tbd" alt="tbd" />
+        </div>
+      </div>
+    </div>
+  )
+}
